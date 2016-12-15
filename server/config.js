756c9bc7628c1bc24920d5e866e6f43ef0585e34
justinmarkhamlawrence <justinmarkhamlawrence@gmail.com>
2016-12-15 15:48:59 -0500

    config

--- server/config.js
+++ server/config.js
@@ -1,7 +1,6 @@
-exports.DATABASE_URL = 'mongodb://justinmlawrence:abcd1234@ds113678.mlab.com:13678/german-x';
-                      //   process.env.DATABASE_URL ||
-                      //  global.DATABASE_URL ||
-                      //  (process.env.NODE_ENV === 'production' ?
-                      //       'mongodb://localhost/germanx' :
-                      //       'mongodb://localhost/germanx-dev');
-exports.PORT = process.env.PORT || 3000;
+exports.DATABASE_URL = //'mongodb://justinmlawrence:abcd1234@ds113678.mlab.com:13678/german-x';
+                        process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
+                       (process.env.NODE_ENV =
+                            'mongodb://justinmlawrence:abcd1234@ds113678.mlab.com:13678/german-x');
+exports.PORT = process.env.PORT || 5000;
